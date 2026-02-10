00c2c68a55b7cefac6c87135f91851c2f9113796
kaitlintomkosky <kaitlintomkosky@gmail.com>
2026-02-10 15:23:38 -0700

    Make field an array

--- api/lead-capture.js
+++ api/lead-capture.js
@@ -70,7 +70,7 @@ export default async function handler(req, res) {
     });
     return res.status(200).json({
       success: false,
-      errors: field,
+      errors: [field],
     });
     }
   }
